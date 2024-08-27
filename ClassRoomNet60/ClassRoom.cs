43031ab539fb43a386429e6790ba31a8d4c60adc
Daniyal Hussain <dah001@edu.zealand.dk>
2024-08-27 14:28:05 +0200

    classroom

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+        private string _className;
+
+        public DateTime _semesterStart;
+
+        public DateTime SemesterStart
+        {
+            get { return _semesterStart; }
+            set { _semesterStart = value; }
+        }
+        public string Classname
+        {
+            get { return _className; }
+            set { _className = value; }
+        }
+
+        public List<Student> Studentlist;
+
+        public ClassRoom(DateTime semesterStart, string classname)
+        {
+            SemesterStart = semesterStart;
+            Classname = classname;
+        }
+        
---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+        //Instansfelter
+        private string _name;
+        private int _birthmonth;
+        private int _birthday;
+
+        //Properties
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int Birthmonth
+        {
+            get { return _birthmonth; }
+            set { _birthmonth = value; }
+        }
+
+        public int Birthday
+        {
+            get { return _birthday; }
+            set { _birthday = value; }
+        }
+        //Constructor
+        public Student(string name, int birthmonth, int birthday)
+        {
+            _name = name;
+            _birthmonth = birthmonth;
+            _birthday = birthday;
+        }
+
+    }
+}
